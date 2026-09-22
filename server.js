d71c4d5aff1b2702152fe70e6d27e1c4d748a912
jhutson256 <hut24007@byui.edu>
2026-09-21 18:14:18 -0600

    Templating Engine completed

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 3000;
 
 /**
@@ -24,18 +25,27 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
 /**
  * Declare Routes
  */
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+    const title = 'Welcome Home';
+    res.render('home', { title });
 });
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+    const title = 'About Me';
+    res.render('about', { title });
 });
 app.get('/products', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+    const title = 'Our Products';
+    res.render('products', { title });
 });
 
 // Start the server and listen on the specified port
